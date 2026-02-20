db70ece46594d9257016ff483c3131b5257f9718
Jose <josealbertop11molino@gmail.com>
2026-02-19 22:27:03 -0600

    idk

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,32 +1,48 @@
 module Helper exposing (..)
-type GradeStatus = Approved | Failed | Pending
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
 
 getcategoricalGrade : Float -> GradeStatus
 getcategoricalGrade number =
     if number < 0 then
-       Pending
+        Pending
     else if number >= 7 then
         Approved
     else
         Failed
 
+
 categoricalGrade : List Float -> List GradeStatus
 categoricalGrade collection =
     List.map getcategoricalGrade collection
 
-type AirplaneStatus = OnTime | Boarding | Delayed | Cancelled
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
 
 airplaneScheduleAction : AirplaneStatus -> String
 airplaneScheduleAction status =
     case status of
         Delayed ->
             "Esperar"
+
         OnTime ->
             "Esperar"
+
         Boarding ->
             "Buscar boleto"
+
         Cancelled ->
             "Pedir reembolso"
+
 
 airportAction : List AirplaneStatus -> List String
 airportAction collection =
